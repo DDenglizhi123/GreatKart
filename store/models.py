711df9d6e28aaf7607273eeb03a625f0dd2ca27e
DDenglizhi123 <128276398+DDenglizhi123@users.noreply.github.com>
2026-01-01 19:58:49 +0300

    update

--- store/models.py
+++ store/models.py
@@ -4,8 +4,8 @@ from category.models import Category
 
 # Create your models here.
 class Product(models.Model):
-    product_name = models.CharField(max_length=100, unique=True)
-    slug = models.SlugField(max_length=100, unique=True)
+    product_name = models.CharField(max_length=200, unique=True)
+    slug = models.SlugField(max_length=200, unique=True)
     description = models.TextField(max_length=500, blank=True)
     price = models.IntegerField()
     images = models.ImageField(upload_to="photos/products/", blank=True)
